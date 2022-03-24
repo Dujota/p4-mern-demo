f0a76f8f970152706656262b20c4a7aa1886eb94
dujota <denisdujota@gmail.com>
2022-03-23 22:04:52 -0400

    setup the list index and add procfile for deplpoy

--- controllers/orders.js
+++ controllers/orders.js
@@ -9,9 +9,10 @@ module.exports = {
 async function create(req, res) {
     try {
         // 1. put the order in the database (the data will be incoming via `req.body`)
+        console.log('Line items',req.body.lineItems);
         await OrderModel.create({ lineItems: req.body.lineItems })
         // 2. send a response to frontend - typically we send back the newly created order, or all the list of orders, or just an 'ok'
-        res.status(200).json('ok. Order added to DB!')
+        res.status(200).json('ok. Order added to DB!' + req.body.lineItems)
     } catch (err) {
         res.json(err);
     }
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // State
   const [lineItem, setLineItem] = useState('');
+  const [orders, setOrders] = useState([])
+
+  // handlers
   const handleSubmit = async e => {
     e.preventDefault()
 
@@ -17,20 +21,45 @@ function App() {
 
       const res = await fetch(url, options);
       const newOrder = await res.json()
+      fetchOrders()
       window.alert(newOrder)
+
     } catch (error) {
       console.error(error);
     }
   }
+
+  const fetchOrders = async () => {
+    try {
+      const jsonOrders = await fetch('/api/orders')
+      const orderRes = await jsonOrders.json()
+      setOrders(orderRes.orders)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(
+    () => {
+      // fetch my list
+      fetchOrders()
+    }, [] // empty array says on mount run once
+  )
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <label htmlFor="lineItem">Line Item</label>
         <input type="text" id="lineItem" onChange={e => setLineItem(e.target.value)} value={lineItem}/>
-
         <button type="submit">Add Line Item</button>
       </form>
+
+      {orders.length
+        ?
+          orders.map( order => <li key={order._id}>{order.lineItem}</li>)
+        :
+          <p>Add some orders please!!</p>
+      }
     </div>
   );
 }
